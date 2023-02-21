b22917a03d2f3f502b91bc44dad3d2504b9ace82
marlyne <=>
2023-02-21 12:21:19 +0100

    fixed bug

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,25 +11,25 @@ class FileStorage:
 
     def all(self):
         """returns the dictionary __objects"""
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """serializes __objects to the JSON file"""
-        with open(self.__file_path, "w") as f:
-            d = {k: v.to_dict() for k, v in self.__objects.items()}
+        with open(FileStorage.__file_path, "w") as f:
+            d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
             json.dump(d, f)
 
     def reload(self):
         """deserializes the JSON file to __objects"""
         try:
-            with open(self.__file_path, "r") as f:
+            with open(FileStorage.__file_path, "r") as f:
                 obj_dict = json.load(f)
-                self.__objects = obj_dict
+                FileStorage.__objects = obj_dict
         except FileNotFoundError:
             pass
 
