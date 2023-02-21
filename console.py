826ddc23a97ae4df0dc35ea9835e6c206d7817c5
scorpion00100 <danbetheliryivuze@gmail.com>
2023-02-21 17:57:22 +0100

    add create and show command

--- console.py
+++ console.py
@@ -21,5 +21,37 @@ class HBNBCommand(cmd.Cmd):
         """Handle empty line when is passed as an argument"""
         pass
 
+    def create(self, args):
+        """ceate an instance of Basemodel"""
+        if not args:
+            print("** class name missing **")
+        elif args not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        else:
+            cmd_dm = {'BaseModel': BaseModel}
+            my_obj = cmd_dm[agrs]()
+            my_obj.save()
+            print("{}".format(my_obj.id))
+            storage.save()
+
+    def show(self, line):
+        """printstring representation"""
+        arg = line.split()
+        object_dict = storage.all()
+        if len(arg) == 0:
+            print("** class name missing **")
+        elif arg[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        elif len(arg) == 1:
+            print("** instance id missing **")
+        elif "{}.{}".format(arg[0], arg[1]) not in object_dict:
+            print("** no instance found **")
+        else:
+            print(object_dict["{}.{}".format(arg[0], arg[1])])
+
+    def destroy(self, line):
+        """Deletes an instance based on the class name and id"""
+
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
